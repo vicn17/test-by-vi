decdc520fb5e440c94b46edb18683098c2ac3376
vicn17 <nhutvichung@gmail.com>
2022-11-05 00:11:58 +0700

    upload prev and next phân trang

--- model/js/listPage.js
+++ model/js/listPage.js
@@ -4,7 +4,7 @@ let limitProduct = 9;
 if (screen.width <= 768) limitProduct = 6;
 let listCard = document.querySelectorAll(".card-item");
 
-function loadItem() {
+function loadItem(index) {
   //* Logic phân trang theo giới hạn sản phẩm show ra (limitProduct)
   let beginGetPage = limitProduct * (thisPage - 1);
   let endGetPage = limitProduct * thisPage - 1;
@@ -17,13 +17,22 @@ function loadItem() {
       item.style.display = "none";
     }
   });
-  listPage();
+  listPage(index);
 }
 loadItem();
 
 //*Kiểm tra tổng số trang
-function listPage() {
+function listPage(index) {
   let tatolPage = Math.ceil(listCard.length / limitProduct);
+
+  if (index > 1) {
+    let prev = document.createElement("a");
+    prev.innerText = "<";
+    document.querySelector("#listPage").append(prev);
+    prev.setAttribute("href", "#first-title-home");
+    prev.setAttribute("onclick", "changePage(" + (thisPage - 1) + ")");
+  }
+
   for (let index = 1; index <= tatolPage; index++) {
     let newPage = document.createElement("a");
     newPage.innerText = index;
@@ -34,11 +43,19 @@ function listPage() {
     newPage.setAttribute("onclick", "changePage(" + index + ")");
     newPage.setAttribute("href", "#first-title-home");
   }
+
+  if (index != tatolPage) {
+    let next = document.createElement("a");
+    next.innerText = ">";
+    document.querySelector("#listPage").append(next);
+    next.setAttribute("href", "#first-title-home");
+    next.setAttribute("onclick", "changePage(" + (thisPage + 1) + ")");
+  }
 }
 
 //* Đổi trang
 function changePage(index) {
   thisPage = index;
   document.querySelector("#listPage").innerHTML = "";
-  loadItem();
+  loadItem(index);
 }
